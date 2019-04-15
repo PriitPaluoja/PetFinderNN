1f0063366d47cdbef7fc7733f3969f439c1e9eb8
Priit Paluoja <priitpaluoja@gmail.com>
2019-04-15 13:15:21 +0300

    Initial model

--- main.py
+++ main.py
@@ -2,17 +2,35 @@ import os
 
 import imageio
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 
 data_dir = "data"
 
 # Read train.csv
 # TODO: rm n-rows
-train = pd.read_csv(os.path.join(data_dir, "train.csv"), sep=',', nrows=10)
+train = pd.read_csv(os.path.join(data_dir, "train.csv"), sep=',')
 pet_ids = train["PetID"]
 
 # Read 1 image per pet
 # TODO: there are more than 1 image per pet in the folder.
-image_paths = [os.path.join(data_dir, "train_images", pet_id + "-1.jpg") for pet_id in pet_ids]
-images = [imageio.imread(path) if os.path.isfile(path) else None for path in tqdm(image_paths)]
-train = train.assign(image_1=images)
+# image_paths = [os.path.join(data_dir, "train_images", pet_id + "-1.jpg") for pet_id in pet_ids]
+# images = [imageio.imread(path) if os.path.isfile(path) else None for path in tqdm(image_paths)]
+# train = train.assign(image_1=images)
+
+
+selected_columns = ["Type", "Age", "Breed1", "Breed2", "Gender", "Color1", "Color2", "Color3",
+                    "MaturitySize", "FurLength", "Vaccinated", "Dewormed", "Sterilized", "Health",
+                    "Quantity", "Fee", "State", "VideoAmt", "PhotoAmt", "AdoptionSpeed"]
+
+label_column = "AdoptionSpeed"
+
+y = train["AdoptionSpeed"]
+X = train[selected_columns]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+rf = RandomForestClassifier().fit(X_train, y_train)
+
+print(accuracy_score(y_test, rf.predict(X_test)))
