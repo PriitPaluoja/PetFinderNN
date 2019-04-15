c6d548524b2e62f0eff1cdf11ba14bebe47a6c25
Priit Paluoja <priitpaluoja@gmail.com>
2019-04-15 15:33:18 +0300

    Initial neural-net

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import os
 
 import imageio
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import ElasticNet, LogisticRegression
-from sklearn.metrics import accuracy_score, precision_score, cohen_kappa_score
+from keras import Input, Model
+from keras.layers import Dense
+from keras.optimizers import Adam
+import numpy as np
+import keras.utils
+from sklearn.metrics import accuracy_score, cohen_kappa_score
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-from tqdm import tqdm
 
 data_dir = "data"
 
@@ -23,22 +24,72 @@ pet_ids = train["PetID"]
 # train = train.assign(image_1=images)
 
 
-selected_columns = ["Type", "Age", "Breed1", "Breed2", "Gender", "Color1", "Color2", "Color3",
-                    "MaturitySize", "FurLength", "Vaccinated", "Dewormed", "Sterilized", "Health",
-                    "Quantity", "Fee", "State", "VideoAmt", "PhotoAmt"]
+# "Age"
+# "Fee"
+selected_columns = ["Type",
+                    "Breed1",
+                    "Breed2",
+                    "Gender",
+                    "Color1",
+                    "Color2",
+                    "Color3",
+                    "MaturitySize",
+                    "FurLength",
+                    "Vaccinated",
+                    "Dewormed",
+                    "Sterilized",
+                    "Health",
+                    "State",
+                    "MaturitySize"]
+
+# "VideoAmt"
+# "PhotoAmt"
+# "Quantity"
 
 label_column = "AdoptionSpeed"
 
 y = train[label_column]
-X = train[selected_columns]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
-rf = RandomForestClassifier(n_estimators=5).fit(X_train, y_train)
+X = pd.get_dummies(train[selected_columns], columns=selected_columns)
 
-predictions = rf.predict(X_test)
+# Normalize age:
+to_normalize = ["Age", "Fee", "Quantity"]
+for to_norm in to_normalize:
+    X[to_norm] = (train[to_norm] - train[to_norm].mean()) / train[to_norm].std()
 
-print("Kappa on train: {}".format(round(cohen_kappa_score(y_train, rf.predict(X_train), weights="quadratic"), 4)))
-print("Accuracy on train: {}".format(round(accuracy_score(y_train, rf.predict(X_train), 4))))
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+print(X_train)
+
+
+def get_neural(X, y):
+    # def metric(y_true, y_pred):
+    # return cohen_kappa_score(y_true, y_pred, weights="quadratic")
+
+    inputs = Input(shape=(len(list(X)),))
+    x = Dense(500, activation='relu')(inputs)
+    x = Dense(500, activation='relu')(x)
+    x = Dense(500, activation='relu')(x)
+    x = Dense(500, activation='relu')(x)
+    x = Dense(500, activation='relu')(x)
+    x = Dense(500, activation='relu')(x)
+    predictions = Dense(5, activation='softmax')(x)
+
+    model = Model(inputs=inputs, outputs=predictions)
+    model.compile(optimizer=Adam(lr=0.01), loss='categorical_crossentropy', metrics=['accuracy'])
+
+    model.fit(X, keras.utils.to_categorical(y), batch_size=200, epochs=5, validation_split=0.04)
+    return model
+
+
+model = get_neural(X_train, y_train)
+test_predictions = model.predict(X_test)
+test_predictions = [np.argmax(pred) for pred in test_predictions]
+print(test_predictions)
+
+train_pred = [np.argmax(pred) for pred in model.predict(X_train)]
+
+print("Kappa on train: {}".format(round(cohen_kappa_score(y_train, train_pred, weights="quadratic"), 4)))
+print("Accuracy on train: {}".format(round(accuracy_score(y_train, train_pred), 4)))
 print("________________")
-print("Kappa on test: {}".format(round(cohen_kappa_score(y_test, predictions, weights="quadratic"), 4)))
-print("Accuracy on test: {}".format(round(accuracy_score(y_test, predictions), 4)))
+print("Kappa on test: {}".format(round(cohen_kappa_score(y_test, test_predictions, weights="quadratic"), 4)))
+print("Accuracy on test: {}".format(round(accuracy_score(y_test, test_predictions), 4)))
